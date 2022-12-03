7b23e791450c1aaaf3644e50a078c3c5b0077e61
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-12-03 15:32:11 +0900

    feat: News コンポーネントを外部リンクに変更

--- components/News.tsx
+++ components/News.tsx
@@ -1,7 +1,24 @@
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
+
 export default function News({ news }) {
   return (
-    <div className="p-3">
-      <div className="font-bold">{news.title}</div>
-    </div>
+    <a href={news.url} target="_blank" rel="noreferrer">
+      <div className="p-3 flex items-center space-x-3 hover:bg-gray-200">
+        <div>
+          <div className="flex items-center space-x-3 text-gray-500">
+            <div>{news.source.name}</div>
+            <ArrowTopRightOnSquareIcon className="h-4 w-4" />
+          </div>
+          <div className="font-bold">{news.title}</div>
+        </div>
+        {news.urlToImage && (
+          <img
+            className="h-12 w-12 object-cover rounded-xl"
+            src={news.urlToImage}
+            alt={news.title}
+          />
+        )}
+      </div>
+    </a>
   )
 }
--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -12,11 +12,12 @@ export default function Widgets({ newsResults }) {
           className="pl-10 rounded-full w-full"
         />
       </div>
-      <div className="bg-gray-100 rounded-lg m-3">
+      <div className="bg-gray-100 rounded-xl m-3">
         <div className="p-3 text-xl font-bold">いまどうしてる？</div>
         {newsResults.map((news, index) => (
           <News key={index} news={news} />
         ))}
+        <div className="p-3">さらに表示</div>
       </div>
     </div>
   )
